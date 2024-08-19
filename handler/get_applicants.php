9db3af8153e449071cdc21c5b9fed0a247bf4924
usamataleb <mamenmasau@gmail.com>
2024-08-19 12:34:22 +0300

    Applicants Query Correction

--- handler/get_applicants.php
+++ handler/get_applicants.php
@@ -4,15 +4,14 @@ include_once '../../handler/dbconfig.php';
 // Check if opportunityID is provided
 if (isset($_GET['id'])) {
     $opportunityID = intval($_GET['id']);
-
-    // Fetch applicants for the specific opportunity ID
-    $query = "SELECT * FROM opportunity  WHERE opportunityID = :opportunityID";
+    $query = "SELECT * FROM opportunity o, applicants a WHERE o.opportunityID = o.opportunityID AND o.opportunityID :opportunityID";
     $stmt = $conn->prepare($query);
     $stmt->bindParam(':opportunityID', $opportunityID, PDO::PARAM_INT);
+
 } else {
-    // Fetch all applicants if no opportunity ID is provided
-    $query = "SELECT * FROM credentials";
+    $query = "SELECT * FROM aplicants";
     $stmt = $conn->prepare($query);
+    
 }
 
 $stmt->execute();
